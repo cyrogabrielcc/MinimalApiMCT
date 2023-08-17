0bf2308641d13714fdd819659251f2637edcc0cd
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-17 17:46:23 -0300

    AddDbContext

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using MinimalApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("DBTarefas")); 
+
 // Add services to the container.
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
