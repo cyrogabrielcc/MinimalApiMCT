418aa09b8361a6714cc21684113d4bca06f80884
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-21 12:51:09 -0300

    context

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.models;
+
+namespace MinimalApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public  DbSet<Categoria> Categorias { get; set;}
+        public  DbSet<Produto> Produtos { get; set;}
+    }
+}
