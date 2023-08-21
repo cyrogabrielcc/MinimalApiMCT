b75eca35aca070a26a361a61e3c869cba3155408
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-21 12:38:46 -0300

    models

---
+++ models/Produto.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinimalApi.models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set;}
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DatacCompra { get; set; }
+        public int Estoque { get; set; }
+
+        // propriedades de navegacao
+        public int CategoriaId { get; set; }
+        public Categoria categoria{ get; set; }
+    }
+}
