a1be95d19f201f6e425102656d4461e03c128f04
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-22 21:18:09 -0300

    mappost

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MinimalApi.Context;
+using MinimalApi.models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,16 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Criando o método Get
+app.MapGet("/", () => "Catálogo de produtos - 2022");
+app.MapPost("/Categorias", async (Categoria categoria, AppDbContext db) => 
+{
+    db.Categorias.Add(categoria);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
+});
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -19,5 +30,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();   
 }
+
+
 
 app.Run();
