4ba04c4e2583cba2700f2d0c0aae7a95167ead4f
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-25 11:03:08 -0300

    get all and id

--- Program.cs
+++ Program.cs
@@ -34,6 +34,25 @@ app.MapGet("/categorias/{id:int}",  async (int id, AppDbContext db) => {
             Results.Ok(categoria) : Results.NotFound();
 });
 
+// Ataulizando com o método PUT
+app.MapPut("/categorias/{id:int}", async (int id, Categoria categoria, AppDbContext db)=>{
+
+    // Vendo se o id já existe
+    if (categoria.CategoriaId == id ) return Results.BadRequest();
+    
+    var categoriaDB = await db.Categorias.FindAsync(id);
+
+    if (categoriaDB is null) return Results.NotFound();
+
+    categoriaDB.Nome =categoria.Nome;
+    categoriaDB.Descricao =categoria.Descricao;
+    
+    await db.SaveChangesAsync();
+    return Results.Ok(categoria);
+
+
+});
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
