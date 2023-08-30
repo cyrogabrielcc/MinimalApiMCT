c57c7e79e48348f4f1227d6a28336580c6f79f6f
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-30 12:19:16 -0300

    Novo README

--- Program.cs
+++ Program.cs
@@ -1,16 +1,12 @@
-using System.Diagnostics.Metrics;
-using System.Reflection.Metadata;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using MinimalApi.Context;
 using MinimalApi.models;
 using MinimalApi.Services;
-using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
 
