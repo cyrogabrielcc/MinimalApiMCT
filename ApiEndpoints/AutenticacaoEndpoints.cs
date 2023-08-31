7e0ebdb8a3db99bcc375f0527ca546de95593d94
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-31 10:15:07 -0300

    reorganizando

---
+++ ApiEndpoints/AutenticacaoEndpoints.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using MinimalApi.models;
+using MinimalApi.Services;
+
+namespace MinimalApi.ApiEndpoints
+{
+    public static class AutenticacaoEndpoints
+    {
+        public static MapAuthenticacaoEndpoints(this WebApplication app)
+        {
+            app.MapPost("/login", [AllowAnonymous] (UserModel userModel, ITokenService tokenService) =>
+            {
+                if (userModel == null)
+                {
+                    return Results.BadRequest();
+                }
+
+                if (userModel.UserName == "Cyro" && userModel.Password == "Xyz@1230")
+                {
+                    var tokenString = tokenService.GerarToken(
+                            app.Configuration["Jwt:Key"],
+                            app.Configuration["Jwt:Issuer"],
+                            app.Configuration["Jwt:Audience"],
+                            userModel);
+                    return Results.Ok(new { token = tokenString });
+                }
+
+                else
+                {
+                    return Results.BadRequest("Login inválido");
+                }
+
+            }).Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status200OK)
+            .WithName("Login")
+            .WithTags("Autenticacao");
+        }
+    }
+}
