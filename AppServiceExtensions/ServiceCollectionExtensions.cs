2f0c83ed0ba1380a01b618210574948067f8a4d7
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-09-01 09:16:04 -0300

    Separando o swagger

--- AppServiceExtensions/ServiceCollectionExtensions.cs
+++ AppServiceExtensions/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace MinimalApi.AppServiceExtensions
 {
@@ -9,7 +10,42 @@ namespace MinimalApi.AppServiceExtensions
     {
         public static WebApplicationBuilder AppApiSwagger(this WebApplicationBuilder builder)
         {
+            builder.Services.AddSwagger();
+            return builder;
+        }
 
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "apiagenda", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme.\r\n\r\n Enter 'Bearer'[space] and then your token in the text input below. \r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                            {
+                                {
+                                    new OpenApiSecurityScheme
+                                    {
+                                        Reference = new OpenApiReference
+                                        {
+                                            Type = ReferenceType.SecurityScheme,
+                                            Id = "Bearer"
+                                        }
+                                    },
+                                    new string[] {}
+                                }
+                            });
+            });
+            return services;
         }
     }
 }
