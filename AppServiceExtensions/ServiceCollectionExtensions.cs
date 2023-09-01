4aecdfaedd8e914c037cde9ee06f430b502b415e
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-09-01 09:08:40 -0300

    Separando o swagger

---
+++ AppServiceExtensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinimalApi.AppServiceExtensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static WebApplicationBuilder AppApiSwagger(this WebApplicationBuilder builder)
+        {
+
+        }
+    }
+}
