18a8e9bf6b98a37cc4d5848e6be82f69f01d4cc3
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-09-04 08:22:27 -0300

    Finalizando

--- Program.cs
+++ Program.cs
@@ -1,12 +1,3 @@
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using MinimalApi.Context;
-using MinimalApi.models;
-using MinimalApi.Services;
 using MinimalApi.ApiEndpoints;
 using MinimalApi.AppServiceExtensions;
 
@@ -27,7 +18,5 @@ var environment = app.Environment;
 app.UseExceptionHandling(environment).UseSwaggerMiddlare().UseAppCors();
 app.UseAuthentication();
 app.UseAuthorization();
-
-
 
 app.Run();
